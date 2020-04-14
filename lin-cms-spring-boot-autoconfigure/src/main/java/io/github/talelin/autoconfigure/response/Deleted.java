826bbb9aaddfd2aa0eb6e4f64dcb088a34c93133
Colorful3 <fujiale3@qq.com>
2020-04-14 17:12:51 +0800

    feat: 新增更新成功和删除成功code码

---
+++ lin-cms-spring-boot-autoconfigure/src/main/java/io/github/talelin/autoconfigure/response/Deleted.java
@@ -0,0 +1,31 @@
+package io.github.talelin.autoconfigure.response;
+
+import io.github.talelin.autoconfigure.bean.Code;
+import io.github.talelin.autoconfigure.interfaces.BaseResponse;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 删除相应
+ * @author colorful@TaleLin
+ */
+public class Deleted implements BaseResponse {
+
+    @Getter
+    protected String message = Code.UPDATED.getDescription();
+
+    @Getter
+    protected int code = Code.DELETED.getCode();
+
+    @Getter
+    protected int httpCode = HttpStatus.OK.value();
+
+
+    public Deleted(String message) {
+        this.message = message;
+    }
+
+    public Deleted() {
+    }
+
+}
