a16b2fafad6b06e79edfe4a620d7e51ffc5b276f
桔子 <juzi214032@qq.com>
2020-05-03 12:39:33 +0800

    refactor(LogInterceptor): 去除多余的父类方法调用
    postHandle() 父类方法为空方法体，无需在实现类中调用

--- lin-cms-spring-boot-autoconfigure/src/main/java/io/github/talelin/autoconfigure/interceptor/LogInterceptor.java
+++ lin-cms-spring-boot-autoconfigure/src/main/java/io/github/talelin/autoconfigure/interceptor/LogInterceptor.java
@@ -1,8 +1,8 @@
 package io.github.talelin.autoconfigure.interceptor;
 
+import io.github.talelin.autoconfigure.interfaces.LoggerResolver;
 import io.github.talelin.core.annotation.Logger;
 import io.github.talelin.core.annotation.RouteMeta;
-import io.github.talelin.autoconfigure.interfaces.LoggerResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +16,7 @@ import java.lang.reflect.Method;
  * 行为日志拦截器
  *
  * @author pedro@TaleLin
+ * @author Juzi@TaleLin
  */
 public class LogInterceptor extends HandlerInterceptorAdapter {
 
@@ -29,21 +30,19 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
      * @param response     响应
      * @param handler      处理器
      * @param modelAndView 视图
-     * @throws Exception 异常
      */
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
         // 记录日志
         if (handler instanceof HandlerMethod) {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             Method method = handlerMethod.getMethod();
             Logger logger = method.getAnnotation(Logger.class);
-            RouteMeta meta = method.getAnnotation(RouteMeta.class);
             if (logger != null) {
+                RouteMeta meta = method.getAnnotation(RouteMeta.class);
                 // parse template and extract properties from request,response and modelAndView
                 loggerResolver.handle(meta, logger, request, response);
             }
         }
-        super.postHandle(request, response, handler, modelAndView);
     }
 }
